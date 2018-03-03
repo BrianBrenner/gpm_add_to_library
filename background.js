22a6e12c921c7c7a74f2f08ec503e3dc5caa6dcb
DoubleB123 <bbrenner321@gmail.com>
2018-03-02 17:08:25 -0800

    updated comments and removed unnecessary permissions

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// Finds the tab containing google play and 
+// Sends query to find tabs with google play url and returns a promise
 function getPlayTab(){
     const playTabPromise = browser.tabs.query({url: "*://play.google.com/music/*"});;
     return playTabPromise;
 }
 
-// Listens for user to click on the icon, then executes script in the chrome tab
-browser.browserAction.onClicked.addListener(function(activeTab){
+// Listens for user to click on the icon, then executes script in the firsttab with google play a url
+browser.browserAction.onClicked.addListener(event => {
     const playTabPromise = getPlayTab();
     playTabPromise.
         then(output => {
--- content_scripts/addToLibrary.js
+++ content_scripts/addToLibrary.js
@@ -2,14 +2,20 @@
 // Content script to add current playing song to your library
 (function addToLibrary(){
     console.log('clicked');
+    // the 3 dots near the play button
     const menuButton = document.querySelector('[data-id=now-playing-menu]');
+    // have to click the button because it changes some of the html
     menuButton.click();
+    // the menu that pops up when click the 3 buttons
     const menu = document.querySelector('.song-menu');
+    // this is the html attribute that changes depending the element you are hovering over
     menu.setAttribute('aria-activedescendant', ':a');
+    // this is the specific table element for add to library
     const libraryElement = document.querySelector('#\\:a');
 
     const down = new MouseEvent('mousedown',{bubbles:true, cancelable: true, view: window});
     const up = new MouseEvent('mouseup',{bubbles:true, cancelable: true, view: window});
+    // click the element, event listens for mouse up and down, not click for some reason
     libraryElement.dispatchEvent(down);libraryElement.dispatchEvent(up);
 })();
 
